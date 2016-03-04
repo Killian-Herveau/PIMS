2fd296d938c7e220170c8f727004d41ab577b81a
BriseisV <briseis.varin@gmail.com>
2016-03-04 12:17:43 +0100

    Fin de semaine 4

---
+++ Code/approxgauss2D2.m
@@ -0,0 +1,18 @@
+function [ p ] = approxgauss2D2( data,p0,dim_h,dim_v, poids )
+%approxsinc: renvoie les coefficients correspondant à l'approximation d'une
+%gaussienne avec offset
+%de la forme y=A+Bexp((x-D)^2)/(2s*s)
+%Entrees:
+%   x et y: données à approximer
+%   p0: coefficients autours desquels on va chercher la bonne approximation
+%       p0(1) -> offset
+%       p0(2) -> amplitude
+%       p0(3) -> moyenne (horizontale)
+%       p0(4) -> moyenne (verticale)
+%       p0(5) -> écart-type
+%%
+options = optimoptions('lsqnonlin','Jacobian','on');
+costFunction = @(p) poids.*(Rgauss2D(data,p,dim_h,dim_v)-data);
+p=lsqnonlin(costFunction,p0);
+
+end
