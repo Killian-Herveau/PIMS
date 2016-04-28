dcf2917d59cbcd89942d1b22944749d9dba9413f
Adrien <adrienmau@orange.fr>
2016-04-28 10:36:35 +0200

    pres

--- Code/todelete.m
+++ Code/todelete.m
@@ -7,5 +7,9 @@ subplot(2,3,1)
 imshow2(cal1(a:b,a:b))
 title('image initiale')
 
-hg=hgauss
+subplot(2,3,2)
+hg=hgaussp([120,120],[61,61],1,28,0,10);
+hg2=hgaussp([120,120],[61,61],1,10,0,10);
+imshow2(hg-hg2)
+title('donuts hypergaussien')
 
