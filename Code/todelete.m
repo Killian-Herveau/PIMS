78f2107a3317b13ec0059366a0f8869a3629de9a
Adrien <adrienmau@orange.fr>
2016-04-28 10:30:24 +0200

    pres

--- Code/todelete.m
+++ Code/todelete.m
@@ -1,6 +1,11 @@
-plot(y)
-hold on
-plot(hcal)
-xlabel('position pixel')
-ylabel('valeur pixel')
-title('Fit gaussien')
+cal1=bin2mat('D:\FarView\Projet 2A\Calibration\STACK=0001_IM=00001_Z=000700.2Ddbl');
+cal1=masque_rephase(cal1)
+a=50;
+b=71;
+
+subplot(2,3,1)
+imshow2(cal1(a:b,a:b))
+title('image initiale')
+
+hg=hgauss
+
